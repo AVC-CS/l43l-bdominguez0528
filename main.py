ecd29bf74b9600d1145a1a6487348ff275351c9c
Bryant Dominguez <bdominguez0528@gmail.com>
2025-04-11 20:52:43 -0700

    modified main.py

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ def main():
     numbers = [0] * 5
     for i in range(len(numbers)):
         numbers[i] = int(input('Enter a value: '))
-
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+        total += numbers[i]
+        
 
     # total = sum(numbers)
     print(total)
